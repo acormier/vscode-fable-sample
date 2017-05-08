952fe330c24fdd13cbd6f9c3529ed424b5aeb2dd
Vasyl Purchel <vasyl.purchel@workday.com>
2017-05-09 00:03:14 +0100

    Upgrade to use fable-import-vscode@0.1.0-narumi-2
    Change-Id: Id61bd75c35f1c0eadfc43a9f136ff6969ad7196f

--- src/extension.fs
+++ src/extension.fs
@@ -11,15 +11,17 @@ let activate(context: ExtensionContext) =
   let wordCount (d: TextDocument) =
     System.Text.RegularExpressions.Regex(@"\w+").Matches(d.getText()).Count
 
-  let showIfMdElseHide() =
-    let e = window.activeTextEditor
-    let d = e.document
-    if d.languageId = "markdown"
-      then 
-        sb.text <- wordCount d |> sprintf "word count: %d"
-        sb.show()
-      else 
-        sb.hide()
+  let showIfMdElseHide(ev) =
+    match window.activeTextEditor with
+    | Some e ->
+        let d = e.document
+        if d.languageId = "markdown"
+          then 
+            sb.text <- wordCount d |> sprintf "word count: %d"
+            sb.show()
+          else
+            sb.hide()
+    | _ -> printf "Active text editor is undefined"
 
   showIfMdElseHide()
 
